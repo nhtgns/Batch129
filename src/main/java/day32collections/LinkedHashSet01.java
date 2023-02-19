07c7ecbd60afc20ae82f2557b34dc11af8d9c724
nhtgns <gunesnihat.99@gmail.com>
2023-02-19 17:15:41 +0100

    Hashset

--- src/main/java/day32collections/LinkedHashSet01.java
+++ src/main/java/day32collections/LinkedHashSet01.java
@@ -5,17 +5,16 @@ import java.util.LinkedHashSet;
 public class LinkedHashSet01 {
     public static void main(String[] args) {
 
-        //LinkehHasset olusturma
+        //LinkehHasset olusturma  //"Set"ler tekrarsiz eleman icerirler.
         LinkedHashSet< String> artist = new LinkedHashSet<>();
 
         artist.add("Ajda");
         artist.add("Tom");
         artist.add("Bradd");
         artist.add("Angelina");
-         //"Set"ler tekrarsiz eleman icerirler.
-        //Siz ayni elemani tekrar tekrar eklerseniz Java hata vermez, ama tekrarli eklenen elemani "Set" e sadece bir kere koyar.
-        artist.add("Tom");
-        System.out.println(artist);  //[Ajda, Tom, Bradd, Angelina] verilen siraya gore dizdi
+
+        artist.add("Tom");//Siz ayni elemani tekrar tekrar eklerseniz Java hata vermez, ama tekrarli eklenen elemani "Set" e sadece bir kere koyar.
+        System.out.println(artist);  //[Ajda, Tom, Bradd, Angelina] verilen siraya gore dizdi.2 elemandan enson olan i yazdirir, buna override denir
 
         LinkedHashSet<String> actress = new LinkedHashSet<>();
         actress.add("Ajda");
@@ -24,6 +23,12 @@ public class LinkedHashSet01 {
         artist.removeAll(actress);
         System.out.println(artist); //[Tom, Bradd]
 
+//        LinkedHashSet<String> has = new LinkedHashSet<>();
+//        has.add("Bradd");
+//        has.add("Tom");
+//
+//
+//        System.out.println(artist.addAll(has)); //addAll methodunu ogren
 
     }
 }
--- src/main/java/day32collections/TreeSet01.java
+++ src/main/java/day32collections/TreeSet01.java
@@ -1,6 +1,7 @@
 package day32collections;
 
 import java.util.HashSet;
+import java.util.NavigableSet;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
@@ -9,30 +10,33 @@ public class TreeSet01 {
 
 
         long one = System.nanoTime();
-        TreeSet<Integer> ts = new TreeSet<>();
 
+        TreeSet<Integer> ts = new TreeSet<>();
         ts.add(12);
         ts.add(25);
         ts.add(8);
         ts.add(32);
         ts.add(3);
-        System.out.println(ts);// [3, 8, 12, 25, 32]
+        System.out.println(ts);// [3, 8, 12, 25, 32]  naturiel order
+
+        long two = System.nanoTime();
 
         HashSet<Integer> hs = new HashSet<>();
-        long two = System.nanoTime();
         hs.add(12);
         hs.add(25);
         hs.add(8);
         hs.add(32);
         hs.add(3);
-        System.out.println(hs); //[32, 3, 8, 25, 12]
+        TreeSet<Integer> hts = new TreeSet<>(hs); //burada hashset ,treeset e cevrildi, treeset in parantezine hashset in hs si yazilarak
+        System.out.println(hts); //[32, 3, 8, 25, 12]  random ,rastgele, sonradan naturel order oldu  [3, 8, 12, 25, 32]
 
         long three = System.nanoTime();
 
-        System.out.println("Treeset :" + (two -one));
+        System.out.println("Treeset in uygulama suresi :" + (two   - one));
+        System.out.println("Hashset in uygulama suresi :" + (three - two));
         System.out.println();
 
-        //Note: TreeSet eleman ekleme'de cooook yavas, HashSet is cooook hizlidir.
+        //Note: TreeSet eleman ekleme'de cooook yavas, HashSet ise cooook hizlidir.
         // TreeSet'in bu negatif yonunden kurtulmak icin; HashSet olusturur elemanlari ekler, ve sonra HashSet'i TreeSet'e ceviririz.
 
         int first = ts.first();
@@ -41,25 +45,42 @@ public class TreeSet01 {
         int last = ts.last();
         System.out.println(last); //32
 
-        int floor = ts.floor(15);
-        System.out.println(floor);  //12 == 15 elemanlardan biri degil, o yuzden 15 ten bir onceki eleman yazdirildi
+        int floor = ts.floor(15);  // floor, eleman varsa kendisini yoksa kendisinden onceki elemani yazdirir
+        System.out.println(floor);  //12,    15 elemanlardan biri degil, o yuzden 15 ten bir onceki eleman yazdirildi
 
-        //floor methodunda kullanabileceginiz sayi en kucuk elemandan az olamaz.
+        //floor methodunda kullanabileceginiz sayi en kucuk elemandan az olamaz. olursa nullpointerexceptions verir
         int floor2 = ts.floor(12);
-        System.out.println(floor2);  //12 elemanlardan biri , oyuzden 12 yi yazdirdi
+        System.out.println(floor2);  //12 elemanlardan biri , o yuzden 12 yi yazdirdi
 
-        int ceiling1 = ts.ceiling(15);
-        System.out.println(ceiling1); // 25 elemanlardan biri degil
+        int ceiling1 = ts.ceiling(15); // eleman varsa elemani yoksa kendisinden buyuk sayiyi verir
+        System.out.println(ceiling1); // 25,    15 elemanlardan biri degil
 
         //ceiling() method'unda kullanabileceginiz sayi en buyuk elemandan buyuk olamaz
         int ceiling2 = ts.ceiling(25);
         System.out.println(ceiling2); // 25 elemanlardan biri ,o yuzden direkt yazdirdi
 
-        // 1000 ve ustunu gormek icin ceiling methodu kullanilabilir. 1000 varsa verir ,degilse
+        // 1000 ve ustunu gormek icin ceiling methodu kullanilabilir. 1000 varsa verir ,degilse 1000 den buyuk sayiyi verir
 
         SortedSet<Integer>tailSet1 = ts.tailSet(15);
-        System.out.println(tailSet1);//[25, 32]
+        System.out.println(tailSet1);//[25, 32] varsa kendisi dahil yoksa kendisinden buyuk "tum sayilari" verir
 
+        SortedSet<Integer>tailSet2 = ts.tailSet(8);
+        System.out.println(tailSet2);   //[8, 12, 25, 32]  8 ve sonrakiler set icinde yazdirildi
 
+        NavigableSet<Integer> ts3 =  ts.tailSet(12,false);
+        System.out.println(ts3);//normalde 12 bir eleman ama ben 2.parametrede "false" kullandigim icin 12 haric tutuldu.
+
+        SortedSet<Integer> headset1 =  ts.headSet(12);
+        System.out.println(headset1);//[3, 8]  burada 12 bir eleman ama set icinde sadece oncekiler yazdirildi, 12 yok
+
+        SortedSet<Integer> headset2 =  ts.headSet(12, true);
+        System.out.println(headset2);  //[3, 8, 12]  true deyince 12 dahil oldu
+
+      //  subSet() methodunda bir yerden bir yere kadar alinir, ilk eleman dahil 2. eleman dahil degil
+        Integer higher1=ts.higher(12)  ;              //her data type ile calisan methodlara Generic method denir. burada int verirsen
+        System.out.println(higher1); //25 ,  12 den bir sonraki buyuk elemani verir
+
+        Integer lower = ts.lower(12);
+        System.out.println(lower); //8,  12 den bir onceki elemani verir
     }
 }
