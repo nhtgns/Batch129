bb7863cce03ac4a89ebed1a03e8cae9cebcd515f
nihat <yenigalatasaray1905@gmail.com>
2023-03-04 21:25:30 +0100

    V

---
+++ src/main/java/Arrays05.java
@@ -0,0 +1,9 @@
+public class Arrays05 {
+    public static void main(String[] args) {
+        toplam(3,7);
+    }
+
+    private static void toplam(int a, int b) {
+        System.out.println(a+b);
+    }
+}
