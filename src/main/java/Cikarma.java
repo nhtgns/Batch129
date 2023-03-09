4ad3d32ad491c0a6665a58dddccf23fb60817653
nihat <yenigalatasaray1905@gmail.com>
2023-03-09 23:13:03 +0100

    toplama

--- src/main/java/Cikarma.java
+++ src/main/java/Cikarma.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class Cikarma {
+public class Cikarma {
+    public static void main(String[] args) {
+
+    }
 }
--- src/main/java/Toplama.java
+++ src/main/java/Toplama.java
@@ -2,5 +2,6 @@ public class Toplama {
     public static void main(String[] args) {
         int a;
         int b;
+
     }
 }
